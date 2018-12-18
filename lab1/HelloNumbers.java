8d303a9caa5981d50172a5375445dfe6f7efe241
qian-wu <wuqianlh@gmail.com>
2018-12-18 20:58:30 +0800

    Add LeapYear.java

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0, i = 0;
+		while(i < 10) {
+			x = x + i;
+			System.out.print(x + " ");
+			i = i + 1;
+		}
+	}
+}
--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,20 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author ww
  */
 public class LeapYear {
+
+    /** Check is or not leap year
+     *  @param year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if(year % 400 == 0){
+            return true;
+        }else if(year % 4 == 0 || year % 100 == 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
