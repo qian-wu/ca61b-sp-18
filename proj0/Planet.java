badbadf2dfce367e8952e965354eee43cf0f0e17
ww <wuqianlh@gmail.com>
2018-12-21 10:42:18 +0800

    Set G to private in Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -7,7 +7,7 @@ public class Planet {
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
-	public static double G = 6.67e-11;
+	private static double G = 6.67e-11;
 
 	public Planet(double xP, double yP, double xV, double yV, double m, String img) {
 		xxPos = xP;
