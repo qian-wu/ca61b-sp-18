63c67ca1124a7d4dbfb8064f1083fe1ede023650
qian-wu <wuqianlh@gmail.com>
2019-01-11 23:02:36 +0800

    Proj1a first commit

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -95,7 +95,7 @@ public class ArrayDeque<T> {
 
     public T removeFirst() {
         if (isEmpty()) {
-            return (T) new Integer(19);
+            return null;
         }
         if (isLowCapisity()) {
             shrunk();
@@ -109,7 +109,7 @@ public class ArrayDeque<T> {
 
     public T removeLast() {
         if (isEmpty()) {
-            return (T) new Integer(19);
+            return null;
         }
         if (isLowCapisity()) {
             shrunk();
